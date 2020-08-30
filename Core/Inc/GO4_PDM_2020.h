0da7a70da3b9c8279672799d2614928338ad4094
Ian Colson <colso031@umn.edu>
2020-08-30 17:38:16 -0500

    fixed tabs

--- Core/Inc/GO4_PDM_2020.h
+++ Core/Inc/GO4_PDM_2020.h
@@ -19,25 +19,22 @@
 
 // Typedefs
 typedef enum {
-	CHANNEL_1_DEVICE = 0,
-	CHANNEL_2_DEVICE = 1,
-	CHANNEL_4_DEVCIE = 2
+    CHANNEL_1_DEVICE = 0, CHANNEL_2_DEVICE = 1, CHANNEL_4_DEVCIE = 2
 } CHANNEL_DEVICE;
 
 typedef enum {
-	NORMAL 			= 0,	// Channel withing operating current
-	RESTART_OFF 	= 1,	// Channel off, will attempt to restart
-	PERMANENT_OFF	= 2		// Channel off forever
+    NORMAL = 0,	// Channel withing operating current
+    RESTART_OFF = 1,	// Channel off, will attempt to restart
+    PERMANENT_OFF = 2		// Channel off forever
 } ADC_CHANNEL_STATE;
 
 typedef enum {
-	UNUSED = 0,		// Current data has not been used in a calculation yet
-	USED   = 1,		// Current data HAS been used in a calculation - dont double count
+    UNUSED = 0,		// Current data has not been used in a calculation yet
+    USED = 1,// Current data HAS been used in a calculation - dont double count
 } CURRENT_DATA_STATE;
 
 // Functions
 void PDM_Init(void);
 void Current_Control_Loop(void);
-
 
 #endif /* INC_GO4_PDM_2020_H_ */
